6e76f3c56e8ca3e251e52510dbc9d845b7f59187
CMiller <mlr.chase40@gmail.com>
2022-04-18 03:43:02 -0500

    add layout design and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+const startBtnEl = document.querySelector("#start-btn");
+const quizTimeEl = document.querySelector("#quiz-timer");
+const introEl = document.querySelector(".intro");
+
+
+// FUNCTIONS
+
+// countdown to zero from specified number in one second increments
+const startCountdown = (startNum) => {
+    quizTimeEl.innerText = startNum;
+    let count = startNum;
+    introEl.classList.add("hidden");
+    
+    let countdown = setInterval(() => {
+        if(count > 0){
+            quizTimeEl.innerText = count;
+            count--;
+        } else {
+            quizTimeEl.innerText = count;
+            clearInterval(countdown);
+            introEl.classList.remove("hidden");
+        }
+    }, 1000);
+}
+
+const startHandler = function(event) {
+    startCountdown(10);
+};
+
+
+
+// EVENT LISTENERS
+startBtnEl.addEventListener("click", startHandler);
